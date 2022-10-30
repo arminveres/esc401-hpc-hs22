d0885fa950eb1e798bb84c347e688ddbb53b35ae
Armin Richard Veres <arminrichard.veres@uzh.ch>
2022-10-30 17:24:59 +0100

    Exercise 6, Task 2: Parallel Poisson

--- exercise_session_06/poisson_OMP/jacobi.cpp
+++ exercise_session_06/poisson_OMP/jacobi.cpp
@@ -1,5 +1,10 @@
 #include "jacobi.h"
+
 #include <math.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif  // _OPENMP
 
 /**
  * @brief      Computes norm of the difference between two matrices
@@ -10,14 +15,16 @@
  *
  * @return     Returns \sqrt{\sum(mat1_{ij} - mat2_{ij})^2/(nx*ny)}
  */
-double norm_diff(params p, double** mat1, double** mat2){
+double norm_diff(params p, double** mat1, double** mat2) {
     double sum = 0.0;
-    for (int i=0; i<p.nx; i++){
-    for (int j=0; j<p.ny; j++){
-        sum += (mat1[i][j] - mat2[i][j])*(mat1[i][j] - mat2[i][j]);
+#pragma omp parallel for reduction(+ : sum)
+    for (int i = 0; i < p.nx; i++) {
+        for (int j = 0; j < p.ny; j++) {
+            // #pragma omp critical
+            sum += (mat1[i][j] - mat2[i][j]) * (mat1[i][j] - mat2[i][j]);
+        }
     }
-    }
-    sum /= p.nx*p.ny;
+    sum /= p.nx * p.ny;
     sum = sqrt(sum);
     // printf("Function norm_diff (jacobi.cpp l.12): not implemented.\n");
     return sum;
@@ -32,19 +39,27 @@ double norm_diff(params p, double** mat1, double** mat2){
  * @param      u_old  The old solution
  * @param      f      The source term
  */
-void jacobi_step(params p, double** u_new, double** u_old, double** f){
-    double dx = 1.0/((double)p.nx - 1);
-    double dy = 1.0/((double)p.ny - 1);
-	
-    for (int i=0; i<p.nx; i++){
-        for (int j=0; j<p.ny; j++)
+void jacobi_step(params p, double** u_new, double** u_old, double** f) {
+    double dx = 1.0 / ((double)p.nx - 1);
+    double dy = 1.0 / ((double)p.ny - 1);
+
+#pragma omp parallel for
+    for (int i = 0; i < p.nx; i++) {
+        for (int j = 0; j < p.ny; j++) {
+#pragma omp critical
             u_old[i][j] = u_new[i][j];
+        }
     }
 
-    for (int i=1; i<p.nx-1; i++){
-        for (int j=1; j<p.ny-1; j++)
-            u_new[i][j] = 0.25*(u_old[i-1][j] + u_old[i+1][j] + u_old[i][j-1] + u_old[i][j+1] - dx*dy*f[i][j]);
+#pragma omp parallel for
+    for (int i = 1; i < p.nx - 1; i++) {
+        for (int j = 1; j < p.ny - 1; j++) {
+#pragma omp critical
+            u_new[i][j] = 0.25 * (u_old[i - 1][j] + u_old[i + 1][j] + u_old[i][j - 1] +
+                                  u_old[i][j + 1] - dx * dy * f[i][j]);
+        }
     }
     // printf("Function jacobi_step (jacobi.cpp l.26): not implemented.\n");
-    if (p.nx!=p.ny) printf("In function jacobi_step (jacobi.cpp l.26): nx != ny, check jacobi updates\n");
+    if (p.nx != p.ny)
+        printf("In function jacobi_step (jacobi.cpp l.26): nx != ny, check jacobi updates\n");
 }
