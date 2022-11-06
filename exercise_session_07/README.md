cdb14aaa56095eb247dfd560a9e8f59e8059775c
Armin Richard Veres <arminrichard.veres@uzh.ch>
2022-11-06 13:22:52 +0100

    Exercise 6, task 1: nonblocking ring

--- exercise_session_07/README.md
+++ exercise_session_07/README.md
@@ -2,7 +2,10 @@
 
 ## Rigns and Deadlocks
 
-Without splitting the send and received into two separate parts, there is a deadlock and nothing happens.
+- Without splitting the send and received into two separate parts, there is a deadlock and nothing
+  happens. Splitting the sends and receives into alternating even/odd ranks word if the the non
+  synchronous send methods are used!
 
-Splitting the sends and receives into alternating even/odd ranks word if the the non synchronous
-send methods are used!
+- Since we are in a ring, we always find ourselved in a block (except with alternating sending as seen)
+  sinve we have to wait for the previous sender. With a nonblocking one, we don't have that problem and
+  values can be received independently from having to wait.
---
+++ exercise_session_07/ring_and_deadlocks/nb_ring.c
@@ -0,0 +1,45 @@
+#include <mpi.h>
+#include <stdio.h>
+// #include <openmpi-x86_64/mpi.h>
+
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes and rank of the process
+    int size, my_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    int my_sum = 0;
+
+    int send_rank = my_rank;  // Send    buffer
+    int recv_rank = 0;        // Receive buffer
+
+    // Compute the ranks of left/right neighbours
+    int left_rank = (my_rank + size - 1) % size;
+    int right_rank = (my_rank + 1) % size;
+
+    printf("I am processor %d out of %d, left %d, right %d\n", my_rank, size, left_rank,
+           right_rank);
+
+    MPI_Status status[2];
+    MPI_Request request[2];
+
+    for (int i = 0; i < size; i++) {
+        // MPI_Isend(&send_rank, 1, MPI_INT, right_rank, 0, MPI_COMM_WORLD, &request[0]);
+        // MPI_Irecv(&recv_rank, 1, MPI_INT, left_rank, 0, MPI_COMM_WORLD, &request[1]);
+
+        MPI_Irecv(&recv_rank, 1, MPI_INT, left_rank, 0, MPI_COMM_WORLD, &request[1]);
+        MPI_Isend(&send_rank, 1, MPI_INT, right_rank, 0, MPI_COMM_WORLD, &request[0]);
+
+        MPI_Waitall(2, request, status);
+        send_rank = recv_rank;
+        my_sum += recv_rank;
+    }
+
+    printf("I am processor %d out of %d, and the sum is %d\n", my_rank, size, my_sum);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
